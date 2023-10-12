790df4fe723999d7a516920a777f4194f3da2317
Dell-User <Dell-User@DESKTOP-GCMBBUK>
2023-10-12 19:07:56 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24.Abstact
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vihicle car1 = new Vihicle(); // this is not possible as parent class is astract! to increase security 
+            Corsa car2 = new Corsa();
+                Console.WriteLine(car2.wheels);
+            Console.ReadLine(); 
+        }
+    }
+    abstract class Vehicle
+    {
+        public int wheels = 4;
+    }
+    class Corsa : Vehicle
+    {
+        public int sparewheels = 1;
+    }
+}
